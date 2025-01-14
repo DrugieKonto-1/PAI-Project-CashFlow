32cb717526cda287fd97377a3f44dc480750ef5e
Wiktor <u54_wikwoj_lbn@technischools.com>
2025-01-14 15:28:45 +0100

    Aktualizacja app.tsx

--- cash-flow/src/App.tsx
+++ cash-flow/src/App.tsx
@@ -1,14 +1,6 @@
-import React, { useState } from 'react';
-import { RouterProvider } from 'react-router-dom';
 import { Router } from './components/Router';
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
     <>
     <Router />
