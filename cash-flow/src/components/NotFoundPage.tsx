ca1ab144401128bf1084fcefb43f0142a7633af9
Michał <u57_miczys_lbn@technischools.com>
2025-01-15 13:10:44 +0100

    sigma

---
+++ cash-flow/src/components/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+
+export const NotFoundPage = () => {
+  return (
+    <div className="not-found">
+      <h1>Nie znaleziono strony</h1>
+      <p>Przepraszamy, nie możemy znaleźć strony, której szukasz.</p>
+      <a href="/" className="home-link">
+        Wroc strony głownej.
+      </a>
+    </div>
+  );
+};
