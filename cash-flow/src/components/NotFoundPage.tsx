d1de31cea78d66cb42b360b7ada9bdcdd522650b
Szymon Sidor <szsidor@gmail.com>
2025-01-16 09:52:24 +0100

    dodanie strony 404

--- cash-flow/src/components/NotFoundPage.tsx
+++ cash-flow/src/components/NotFoundPage.tsx
@@ -1,9 +1,12 @@
+import Lottie from 'lottie-react';
+import animationData from '../assets/not-found-animation.json';
 
 export const NotFoundPage = () => {
   return (
     <div className="not-found">
-      <h1>404</h1>
-      <p>Strona, ktorej szukasz nie istnieje.</p>
+      <div className="animation-container">
+        <Lottie animationData={animationData} style={{ width: 700, height: 700 }} />
+      </div>
       <a href="/" className="home-link">
         Wroc go strony głownej.
       </a>
