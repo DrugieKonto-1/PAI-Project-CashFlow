5b1063db93c4cdf8f66d1f79a374cbe0b01f5941
Wiktor <u54_wikwoj_lbn@technischools.com>
2025-01-19 19:39:54 +0100

    Dodanie Fetcha do Api. (W.I.P)

--- cash-flow/src/components/GetStarted.tsx
+++ cash-flow/src/components/GetStarted.tsx
@@ -1,7 +1,9 @@
+import { FetchComponent } from "./FetchComponent"
 import { StartPage } from "./StartPage"
 
 export const GetStarted = () => {
   return(<>
+  <FetchComponent />
   <StartPage />
   </>)
 }
